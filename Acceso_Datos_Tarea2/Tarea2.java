866c13fc356b3a2b922cb355ed35e61713f1540b
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2025-10-31 10:50:26 +0100

    Delete Acceso_Datos_Tarea2/Tarea2.java

--- Acceso_Datos_Tarea2/Tarea2.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.Properties;
-
-public class Tarea2{
-    public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.setProperty("driver", "h2");
-    }
-}
